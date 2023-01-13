f7a14ee02343a72c39f034d28093f2a450824020
BassTheCoder <patryk.nowak.hello@gmail.com>
2023-01-13 17:45:28 +0100

    server with nodemon, home page hello world

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send("this is a server home page.")
+})
+
 app.listen(5000, () => {
     console.log("server started on port 5000.");
 })
