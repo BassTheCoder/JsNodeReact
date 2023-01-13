7df732a9069cf47f0327e08727fd32d6ee7b1801
BassTheCoder <patryk.nowak.hello@gmail.com>
2023-01-14 00:52:48 +0100

    working insert to database from frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
-import './App.css';
-
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Axios from 'axios'
 
 function App() {
+
+  const [restaurantName, setRestaurantName] = useState('');
+  const [restaurantRating, setRestaurantRating] = useState('');
+  const [restaurantReview, setRestaurantReview] = useState('');
+
+  const submitReview = () => {
+    Axios.post('http://localhost:5000/api/submitReview', { 
+      restaurantName: restaurantName, 
+      restaurantRating: restaurantRating, 
+      restaurantReview: restaurantReview 
+    }).then(() => {
+      alert('A review was successfully submitted.')
+    })
+  };
+
+
   return (
 
     <>
@@ -31,12 +47,12 @@ function App() {
 
                 <form id="ratingsForm" data-sb-form-api-token="API_TOKEN">
                   <div class="form-group mb-3">
-                    <input class="form-control mb-3" placeholder="Restaurant name" type="text" id="name" data-sb-validations="required" />
+                    <input class="form-control mb-3" placeholder="Restaurant name" type="text" id="name" data-sb-validations="required" onChange={(e) => { setRestaurantName(e.target.value) }} />
                     <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
                   </div>
 
                   <div class="form-group mb-3">
-                    <select class="form-select" id="rating">
+                    <select class="form-select" id="rating" onChange={(e) => { setRestaurantRating(e.target.value) }}>
                       <option value="" disabled selected>Select your rating</option>
                       <option value="1">1</option>
                       <option value="2">2</option>
@@ -45,7 +61,12 @@ function App() {
                       <option value="5">5</option>
                     </select>
                   </div>
-                  <div class="d-grid"><button class="btn btn-primary btn-xl" id="submitButton" type="submit">Submit your rating</button></div>
+
+                  <div>
+                    <input class="form-control mb-3" placeholder="Restaurant review" type="text" id="review" data-sb-validations="required" onChange={(e) => { setRestaurantReview(e.target.value) }} />
+                    <div class="invalid-feedback" data-sb-feedback="review:required">A review is required.</div>
+                  </div>
+                  <div class="d-grid"><button class="btn btn-primary btn-xl" id="submitButton" type="submit" onClick={submitReview}>Submit your rating</button></div>
 
                 </form>
                 <hr class="divider" />
--- server/server.js
+++ server/server.js
@@ -1,10 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express();
+const mysql = require('mysql');
 
-app.get('/', (req, res) => {
-    res.send("this is a server home page.")
-})
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: '',
+    dbname: 'food_io'
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected to database!");
+});
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/api/submitReview', (req, res) => {
+
+    const restaurantName = req.body.restaurantName
+    const restaurantRating = req.body.restaurantRating
+    const restaurantReview = req.body.restaurantReview
+
+    const insertQuery = "INSERT INTO food_io.restaurant_reviews (restaurant_name, restaurant_rating, restaurant_review) VALUES (?,?,?);"
+    connection.query(insertQuery, [restaurantName, restaurantRating, restaurantReview], function (err, result) {
+        console.log(result)
+    })
+});
+
+
 
 app.listen(5000, () => {
     console.log("server started on port 5000.");
-})
+});
